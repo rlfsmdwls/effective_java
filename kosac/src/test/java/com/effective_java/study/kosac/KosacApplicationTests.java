5fe728245f9f581ef5b6358f3150871c448580eb
gilneungjin <kilnj@smartauth.kr>
2018-10-04 19:50:36 +0900

    spring boot proj added

---
+++ kosac/src/test/java/com/effective_java/study/kosac/KosacApplicationTests.java
@@ -0,0 +1,16 @@
+package com.effective_java.study.kosac;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KosacApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
